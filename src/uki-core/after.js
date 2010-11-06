fff47a0fe5c7214daf2d30a93897f19ed9b8ef4d
bartuer <bartuer@gmail.com>
2010-11-07 01:36:23 +0800

    after.js style

--- src/uki-core/after.js
+++ src/uki-core/after.js
@@ -8,50 +8,48 @@ include('uki.js');
  * @function
  * @param {function()} callback
  */
-uki.after = (function() {
-    var after = function(callback) {
-        callback.huid = callback.huid || uki.guid++;
-        if (after._bound[callback.huid]) return;
-        after._bound[callback.huid] = true;
-        after._queue.push(callback);
-        if (!after._running) after._startTimer();
-    };
-    
-    after._bound = {};
-    after._running = false;
-    after._timer = 0;
+uki.after = (function () {
+  var after = function (callback) {
+    callback.huid = callback.huid || uki.guid++;
+    if (after._bound[callback.huid]) return;
+    after._bound[callback.huid] = true;
+    after._queue.push(callback);
+    if (!after._running) {after._startTimer();}
+  };
+
+  after._bound = {};
+  after._running = false;
+  after._timer = 0;
+  after._queue = [];
+
+  after.start = function () {
+    after._clearTimer();
+    after._running++;
+  };
+
+  after.stop = function () {
+    if (--after._running) {return;}
+    after._runCallbacks();
+  };
+
+  after._runCallbacks = function () {
+    after._clearTimer();
+    var queue = after._queue;
     after._queue = [];
-    
-    after.start = function() {
-        after._clearTimer();
-        after._running++;
-    };
-    
-    after.stop = function() {
-        if (--after._running) return;
-        after._runCallbacks();
-    };
-    
-    
-    
-    after._runCallbacks = function() {
-        after._clearTimer();
-        var queue = after._queue;
-        after._queue = [];
-        after._bound = {};
-        for (var i=0; i < queue.length; i++) queue[i]();
-    };
-    
-    after._startTimer = function() {
-        if (after._timer) return;
-        after._timer = setTimeout(after._runCallbacks, 1);
-    };
-    
-    after._clearTimer = function() {
-        if (!after._timer) return;
-        clearTimeout(after._timer);
-        after._timer = 0;
-    };
-    
-    return after;
+    after._bound = {};
+    for (var i = 0; i < queue.length; i+=1) {queue[i]();}
+  };
+
+  after._startTimer = function () {
+    if (after._timer) {return;}
+    after._timer = setTimeout(after._runCallbacks, 1);
+  };
+
+  after._clearTimer = function () {
+    if (!after._timer) {return;}
+    clearTimeout(after._timer);
+    after._timer = 0;
+  };
+
+  return after;
 })();
