a178d0f811ea47a8966d57042f83abe94e35557c
bartuer <bartuer@gmail.com>
2010-11-07 17:04:23 +0800

    more/utils.js style

--- src/uki-more/more/utils.js
+++ src/uki-more/more/utils.js
@@ -1,13 +1,14 @@
 include('../more.js');
 
 uki.more.utils = {
-    range: function (from, to) {
-        var result = new Array(to - from), idx = 0;
-        for (; from <= to; from++, idx++) {
-            result[idx] = from;
-        };
-        return result;
+  range: function (from, to) {
+    var result = new Array(to - from),
+      idx = 0;
+    for (; from <= to; from += 1, idx += 1) {
+      result[idx] = from;
     }
+    return result;
+  }
 };
 
 uki.extend(uki, uki.more.utils);
